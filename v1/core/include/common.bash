81a2cf11b7fb6d1580063b69c0aa570eb58a868c
na4ma4 <na4ma4@users.noreply.github.com>
2021-07-16 13:48:05 +1000

    fixed common environment variables

--- v1/core/include/common.bash
+++ v1/core/include/common.bash
@@ -4,6 +4,6 @@ if [ -z "${NA4MA4_MF_ROOT:-}" ]; then
     NA4MA4_MF_ROOT="${MF_PROJECT_ROOT}/.makefiles/ext/na4ma4"
 fi
 
-if [[ "$(which na4ma4-build-resource 2>/dev/null)" != "${NA4MA4_MF_ROOT}/lib/core/bin/na4ma4-build-resource" ]]; then
-    PATH="${NA4MA4_MF_ROOT}/lib/core/bin:${PATH}"
+if [[ "$(which na4ma4-build-resource 2>/dev/null)" != "${NA4MA4_MF_ROOT}/core/bin/na4ma4-build-resource" ]]; then
+    PATH="${NA4MA4_MF_ROOT}/core/bin:${PATH}"
 fi
--- v1/core/include/common.mk
+++ v1/core/include/common.mk
@@ -1,2 +1,2 @@
 export NA4MA4_MF_ROOT := $(MF_PROJECT_ROOT)/.makefiles/ext/na4ma4
-export PATH := $(NA4MA4_MF_ROOT)/lib/core/bin:$(PATH)
+export PATH := $(NA4MA4_MF_ROOT)/core/bin:$(PATH)
